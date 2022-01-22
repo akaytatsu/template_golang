1f643cb5a7567e19aa8e43839ad9a4b44a605606
Thiago Freitas <thiagosistemas3@gmail.com>
2022-01-22 18:44:49 +0000

    first commit

---
+++ src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Golang is running!\n"))
+}
+
+func main() {
+
+	r := mux.NewRouter()
+
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
