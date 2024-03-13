8f73378fcffc8220fc90c42191cf5ccf3df10cf0
Thiago Freitas <thiagosistemas3@gmail.com>
2024-03-13 11:11:01 -0300

    Add pagination and admin middleware

--- src/api/middleware/middleware.go
+++ src/api/middleware/middleware.go
@@ -39,3 +39,36 @@ func AuthenticatedMiddleware(usercase usecase_user.IUsecaseUser) gin.HandlerFunc
 		}
 	}
 }
+
+func AdminMiddleware(usercase usecase_user.IUsecaseUser) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// get bearer token from header
+		token := c.Request.Header.Get("Authorization")
+
+		// token := strings.Split(bearerToken, " ")[1]
+
+		user, err := usercase.GetUserByToken(token)
+
+		// check if token is valid
+		if err == nil {
+
+			if !user.IsAdmin {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"message": "Unauthorized",
+				})
+				c.Abort()
+				return
+			}
+
+			// set user to context
+			c.Set("user", *user)
+
+			c.Next()
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
+			})
+			c.Abort()
+		}
+	}
+}
